166d3e608b32fd4e13454d43d7b0ad7929c9a7f0
Lakshmikanta <frontendfixer@gmail.com>
2023-06-25 23:18:07 +0530

    enable strick-mode

--- src/main.jsx
+++ src/main.jsx
@@ -10,13 +10,13 @@ import App from './App';
 import { persistor, store } from './store/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </PersistGate>
-  </Provider>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
 );
