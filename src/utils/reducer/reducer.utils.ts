b579dfcf26abfbfc0d9a0bdc6db9f83548171158
Lakshmikanta Patra <frontendfixer@gmail.com>
2023-07-26 17:00:17 +0530

    create withMatcher function to match every redux action type

--- src/utils/reducer/reducer.utils.ts
+++ src/utils/reducer/reducer.utils.ts
@@ -1,4 +1,24 @@
+/* eslint-disable no-unused-vars */
 import { AnyAction } from 'redux'
+
+type Matchable<AC extends () => AnyAction> = AC & {
+  type: ReturnType<AC>['type'];
+  match(action: AnyAction): action is ReturnType<AC>
+}
+
+export function withMatcher<AC extends ()=> AnyAction & {type: string}>(actionCreator: AC): Matchable<AC>
+export function withMatcher<AC extends (...args : any[])=> AnyAction & {type: string}>(actionCreator: AC): Matchable<AC>
+
+export function withMatcher(actionCreator : Function){
+  // eslint-disable-next-line prefer-destructuring
+  const type = actionCreator().type;
+  return Object.assign(actionCreator, {
+    type,
+    match(action: AnyAction){
+      return action.type === type
+    }
+  })
+}
 
 export type ActionWithPayload<T, P> = {
   type: T
