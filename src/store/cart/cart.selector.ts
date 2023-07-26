6919b7a813fa8d0065439395bd8254797c1d157d
Lakshmikanta Patra <frontendfixer@gmail.com>
2023-07-26 22:13:44 +0530

    convert cartSelector to return cartState

---
+++ src/store/cart/cart.selector.ts
@@ -0,0 +1,23 @@
+import { createSelector } from 'reselect'
+
+import { CartState } from './cart.reducer'
+
+const selectCartReducer = (state): CartState => state.cart
+
+export const selectCartItems = createSelector(
+  [selectCartReducer],
+  (cart) => cart.cartItems
+)
+
+export const selectIsCartOpen = createSelector(
+  [selectCartReducer],
+  (cart) => cart.isCartOpen
+)
+
+export const selectCartCount = createSelector([selectCartItems], (cartItems) =>
+  cartItems.reduce((acc, el) => el.quantity + acc, 0)
+)
+
+export const selectCartTotal = createSelector([selectCartItems], (cartItems) =>
+  cartItems.reduce((acc, el) => el.quantity * el.price + acc, 0)
+)
