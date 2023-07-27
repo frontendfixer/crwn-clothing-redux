7e520c1c3e4e77e6ba387d35c51ff6cd93231933
Lakshmikanta Patra <frontendfixer@gmail.com>
2023-07-27 17:15:24 +0530

    create RootState type and update to get proper redux state type

--- src/store/checkout/checkout.selector.ts
+++ src/store/checkout/checkout.selector.ts
@@ -1,8 +1,10 @@
 import { createSelector } from 'reselect'
 
+import { RootState } from '../store'
 import { CheckoutState } from './checkout.reducer'
 
-const selectCheckoutReducer = (state): CheckoutState => state.checkout
+const selectCheckoutReducer = (state: RootState): CheckoutState =>
+  state.checkout
 
 export const selectCheckout = createSelector(
   [selectCheckoutReducer],
---
+++ src/store/store.ts
@@ -0,0 +1,52 @@
+import { applyMiddleware, compose, createStore, Middleware } from 'redux'
+import logger from 'redux-logger'
+import { PersistConfig, persistReducer, persistStore } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import createSagaMiddleware from 'redux-saga'
+
+import { rootReducer } from './root.reducer'
+import { rootSaga } from './root.saga'
+
+// root-reducer
+export type RootState = ReturnType<typeof rootReducer>
+
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose
+  }
+}
+
+type ExtendedPersistConfig = PersistConfig<RootState> & {
+  whitelist: (keyof RootState)[]
+}
+
+const persistConfig: ExtendedPersistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'],
+}
+
+const sagaMiddleware = createSagaMiddleware()
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+// import { loggerMIddleware } from './middleware/logger';
+// const middleWares = [loggerMIddleware];
+// don't show 'logger' when we are in production
+const middleWares = [
+  process.env.NODE_ENV !== 'production' && logger,
+  sagaMiddleware,
+].filter((middleware): middleware is Middleware => Boolean(middleware))
+
+const composeEnhancer =
+  (process.env.NODE_ENV !== 'production' &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose
+const composedEnhancer = composeEnhancer(applyMiddleware(...middleWares))
+
+export const store = createStore(persistedReducer, undefined, composedEnhancer)
+
+sagaMiddleware.run(rootSaga)
+
+export const persistor = persistStore(store)
