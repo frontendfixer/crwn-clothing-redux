f0872c4bb38280166219cad69bf5ea5e843eda79
Lakshmikanta Patra <frontendfixer@gmail.com>
2023-07-27 17:07:35 +0530

    update rootReducer styling

--- src/store/root.reducer.js
+++ src/store/root.reducer.js
@@ -1,13 +1,13 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from 'redux'
 
-import { cartReducer } from './cart/cart.reducer';
-import { categoriesReducer } from './categories/category.reducer';
-import { checkoutReducer } from './checkout/checkout.reducer';
-import { userReducer } from './user/user.reducer';
+import { cartReducer } from './cart/cart.reducer'
+import { categoriesReducer } from './categories/category.reducer'
+import { checkoutReducer } from './checkout/checkout.reducer'
+import { userReducer } from './user/user.reducer'
 
 export const rootReducer = combineReducers({
   user: userReducer,
   categories: categoriesReducer,
   cart: cartReducer,
   checkout: checkoutReducer,
-});
+})
