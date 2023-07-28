96428787d74b4a1c6fdc11695d00cbab49fe9c5f
Lakshmikanta Patra <frontendfixer@gmail.com>
2023-07-28 17:12:05 +0530

    convert: authentication component to typescript

---
+++ src/routes/authentication/authentication.component.tsx
@@ -0,0 +1,13 @@
+import SignInForm from '../../components/sign-in-form/sign-in-form.component'
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component'
+import AuthenticationContainer from './authentication.styles'
+
+/* eslint arrow-body-style: ["error", "never"] */
+const Authentication = () => (
+  <AuthenticationContainer>
+    <SignInForm />
+    <SignUpForm />
+  </AuthenticationContainer>
+)
+
+export default Authentication
