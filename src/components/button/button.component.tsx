cb784522478c8cc146b1aad3910d0e23ce2c2aac
Lakshmikanta Patra <frontendfixer@gmail.com>
2023-07-28 15:41:28 +0530

    convert button component to typescript

---
+++ src/components/button/button.component.tsx
@@ -0,0 +1,27 @@
+import { ButtonHTMLAttributes, FC, ReactNode } from 'react'
+import { BaseButton, GoogleSignInButton, InvertedButton } from './button.style'
+
+export enum BUTTON_TYPE_CLASSES {
+  base = 'base',
+  google = 'google-sign-in',
+  inverted = 'inverted',
+}
+
+const getButton = (buttonType = BUTTON_TYPE_CLASSES.base): typeof BaseButton =>
+  ({
+    [BUTTON_TYPE_CLASSES.base]: BaseButton,
+    [BUTTON_TYPE_CLASSES.google]: GoogleSignInButton,
+    [BUTTON_TYPE_CLASSES.inverted]: InvertedButton,
+  }[buttonType])
+
+export type ButtonProps = {
+  children: ReactNode
+  buttonType?: BUTTON_TYPE_CLASSES;
+} & ButtonHTMLAttributes<HTMLButtonElement>
+
+const Button = ({ children, buttonType, ...otherProps }: ButtonProps) => {
+  const CustomButton = getButton(buttonType)
+  return <CustomButton {...otherProps}>{children}</CustomButton>
+}
+
+export default Button
